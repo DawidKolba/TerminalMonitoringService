497031f61a9c76236a0080071d78268741bb2bb3
Dawid Kołba <kolba.dawid@gmail.com>
2023-07-18 10:01:55 +0200

    initial commit;

---
+++ TerminalMonitoringService/ProcessMonitoring/ProcessMonitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalMonitoringService.ProcessMonitoring
+{
+    public class ProcessMonitor
+    {
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+        public ProcessMonitor (string ProcessName)
+        {
+            _logger.Info($"Start monitoring process: {ProcessName}");
+        }
+
+    }
+}
---
+++ TerminalMonitoringService/Worker.cs
@@ -0,0 +1,24 @@
+using TerminalMonitoringService.ProcessMonitoring;
+
+namespace TerminalMonitoringService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var pm = new ProcessMonitor("test");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
